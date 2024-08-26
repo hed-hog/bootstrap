b334b2869844441db4f8c7a8cd0d810acbe95912
Joao Rangel <joaohcrangel@gmail.com>
2024-08-26 20:59:39 -0300

    init template

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CacheModule } from '@nestjs/cache-manager';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { PrismaModule } from '@hedhog/prisma';
+
+@Module({
+  imports: [
+    PrismaModule,
+    CacheModule.register(),
+    ThrottlerModule.forRoot([{ ttl: 60000, limit: 10 }]),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
